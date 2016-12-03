06cf9326cec326e9625d269f15689bf15f9923d0
Organatik <organatik@gmail.com>
2016-12-03 23:53:19 +0200

    MapRespo

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'detail',
-  styleUrls: ['./detail.component.css'],
+  styleUrls: ['./detail.component.sass'],
   templateUrl: './detail.component.html'
 })
 export class DetailComponent {
