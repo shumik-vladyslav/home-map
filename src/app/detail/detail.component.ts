e5a41e1a968b995959a983c80fc097e3e95617f3
Organatik <organatik@gmail.com>
2016-12-04 17:22:15 +0200

    Details

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,9 +1,36 @@
-import {Component} from '@angular/core';
-
+import {Component, OnInit, ElementRef} from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'detail',
   styleUrls: ['./detail.component.sass'],
   templateUrl: './detail.component.html'
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
+  constructor(private _elRef: ElementRef){};
+  ngOnInit() {
+
+    $( ".datepick" ).datepicker();
+    $(".profile-drop").click(function () {
+      $(".profile-menu").slideToggle("slow");
+    });
+
+    $(".mobile-drop").click(function () {
+      $(".mobile-menu").slideToggle("slow");
+    });
+    function sticky_relocate() {
+      var window_top = $(window).scrollTop();
+      var div_top = $('.anchor').offset().top;
+      if (window_top > div_top) {
+        $('.form-fixed').addClass('fix-form');
+      } else {
+        $('.form-fixed').removeClass('fix-form');
+        $('.anchor').height(0);
+      }
+    }
+    $(function() {
+      $(window).scroll(sticky_relocate);
+      sticky_relocate();
+    });
+
+  }
 }
