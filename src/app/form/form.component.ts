34e66fff9847d0ba7392728465192756d1fe31ec
Organatik <organatik@gmail.com>
2016-12-05 16:36:11 +0200

    Form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,9 +1,20 @@
-import {Component} from '@angular/core';
-
+import {Component, OnInit, ElementRef} from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'form',
-  styleUrls: ['./form.component.css'],
+  styleUrls: ['./form.component.sass'],
   templateUrl: './form.component.html'
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  constructor(private _elRef: ElementRef){};
+  ngOnInit() {
+    $(".datepicker").datepicker();
+    $(".profile-drop").click(function () {
+      $(".profile-menu").slideToggle("slow");
+    });
+
+    $(".mobile-drop").click(function () {
+      $(".mobile-menu").slideToggle("slow");
+    });
+  }
 }
