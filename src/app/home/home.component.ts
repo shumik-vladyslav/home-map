9b4ded1ab136d0075ecdc60be2f95b37545902bc
Organatik <organatik@gmail.com>
2016-12-12 16:44:29 +0200

    fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,31 +6,6 @@ declare var $: any;
   templateUrl: './home.component.html'
 })
 
-export class HomeComponent implements OnInit{
-  constructor(private _elRef: ElementRef){};
-  showLoad = true;
-  ngOnInit() {
-    setTimeout(() => this.showLoad = false, 8000);
-
-    $("#datepicker").datepicker();
-
-    $("#owl").owlCarousel({
-
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
-
-      items: 4,
-      itemsDesktop: [1199, 3],
-      itemsDesktopSmall: [979, 3]
-
-    });
-    $(".profile-drop").click(function () {
-      $(".profile-menu").slideToggle("slow");
-    });
-
-    $(".mobile-drop").click(function () {
-      $(".mobile-menu").slideToggle("slow");
-    });
-
-}
+export class HomeComponent{
 
 }
